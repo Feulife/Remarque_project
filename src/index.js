8a1ab7eaba9ba26274b7a631ab740f6af23b5515
Andrii Racilewicz <75903440+Feulife@users.noreply.github.com>
2022-07-24 22:53:04 +0300

    add mutation data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
-const app = express();
 const port = process.env.PORT || 4000;
+let notes = [
+  { id: '1', content: 'Script_1', author: 'Programmer' },
+  { id: '2', content: 'Script_2', author: 'Coder' },
+  { id: '3', content: 'Script_3', author: 'Developer'}
+];
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    aythor: String!
+  }
+  
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note: (id: ID!): Note!
+  }
+  
+  type Mutation {
+    newNote(content: String!): Note!
   }`;
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue
+    }
   }
 };
+const app = express();
 const server = new ApolloServer({ typeDefs, resolvers});
 server.applyMiddleware({ app, path: '/Remarque_project'})
 app.listen(port, () => console.log(`GraphQL Server running at http://localhost:${port}$(server.graphqlPath)`));
