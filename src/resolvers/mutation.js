e13621bf442131328258bd3c5c51b8964dd0daa1
Andrii Racilewicz <75903440+Feulife@users.noreply.github.com>
2022-07-31 02:35:06 +0300

    Flow authorization and registration

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -36,5 +36,41 @@ module.exports = {
         new:true
       }
     );
+  },
+  signUp: async (parent, { username, email, password }, { models }) => {
+    email = email.trim().toLoerCase();
+    const hashed = await bcrypt.hash(password, 10);
+    const avatar = gravatar(email);
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed
+      });
+
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) {
+      console.log(err);
+      throw new Error('Error creating account');
+    }
+  },
+  signIn: async (parent, { username, email, password }, { models }) => {
+    if (email) {
+      email = email.trim().toLowerCase();
+    }
+    const user = await models.User.findOne({
+      $or: [{ email }, { username }]
+    });
+
+    if (!user) {
+      throw new AuthenticatioError('Error signing in');
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AuthenticatioError('Error signig in');
+    }
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   }
 }
