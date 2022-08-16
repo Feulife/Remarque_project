5659c8fa734f7b1bd65e0ec12d8f6dba1532251b
Andrii Racilewicz <75903440+Feulife@users.noreply.github.com>
2022-08-16 23:28:58 +0300

    create function app

--- src/App.js
+++ src/App.js
@@ -1,2 +1,12 @@
-// index.js
-// This is the main entry point of our application
+import React from 'react'
+import { ReactDOM } from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Remarque!!!</h1>
+      <p>Welcome to the Remarque application</p>
+    </div>
+  )
+}
+
