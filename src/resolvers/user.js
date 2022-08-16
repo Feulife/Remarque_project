020ccf9f5fc02daf15d6467ed25543a59927160b
Andrii Racilewicz <75903440+Feulife@users.noreply.github.com>
2022-08-16 23:05:38 +0300

    clear old part of project and install new nodes dependences

--- src/resolvers/user.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  notes: async (user, args, { modules }) => {
-    return await modules.Note.find({ author: user._id }).sort({ _id: -1});
-  },
-  favorites: async (user, args, { modules }) => {
-    return await modules.Note.find({ favoritedBy: user._id }).sort({ _id: -1});
-  }
-};
